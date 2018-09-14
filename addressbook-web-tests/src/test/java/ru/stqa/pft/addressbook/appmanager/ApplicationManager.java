8db2d29e4b9c2fa60be1929854efd1a9d4ab22ca
egaraseva <https://stash.esc-hq.ru/login>
2018-09-15 01:12:16 +0300

    added new tests, some changes in project

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,18 +1,19 @@
 package ru.stqa.pft.addressbook.appmanager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
+import ru.stqa.pft.addressbook.model.ContactData;
 import java.util.concurrent.TimeUnit;
+import static org.testng.Assert.assertTrue;
 
 public class ApplicationManager {
 
-  WebDriver driver;
+  private WebDriver driver;
 
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
   private SessionHelper sessionHelper;
+  private boolean acceptNextAlert = true;
 
   public void init() {
     driver = new FirefoxDriver();
@@ -24,8 +25,27 @@ public class ApplicationManager {
     sessionHelper.login("admin", "secret");
   }
 
-  public void logout() {
-    driver.findElement(By.linkText("Logout")).click();
+  public void login(String username, String password) {
+    driver.findElement(By.name("user")).click();
+    driver.findElement(By.name("user")).sendKeys(username);
+    driver.findElement(By.name("pass")).click();
+    driver.findElement(By.name("pass")).sendKeys(password);
+    driver.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  public void fillContactForm(ContactData contactData) {
+    driver.findElement(By.name("firstname")).click();
+    driver.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
+    driver.findElement(By.name("lastname")).click();
+    driver.findElement(By.name("lastname")).sendKeys(contactData.getSurname());
+    driver.findElement(By.name("company")).click();
+    driver.findElement(By.name("company")).sendKeys(contactData.getCompany());
+    driver.findElement(By.name("mobile")).click();
+    driver.findElement(By.name("mobile")).sendKeys(contactData.getPhone());
+    driver.findElement(By.name("email")).click();
+    driver.findElement(By.name("email")).sendKeys(contactData.getEmail());
+    driver.findElement(By.name("address2")).click();
+    driver.findElement(By.name("address2")).sendKeys(contactData.getAddress());
   }
 
   public void stop() {
@@ -38,5 +58,64 @@ public class ApplicationManager {
 
   public NavigationHelper getNavigationHelper() {
     return navigationHelper;
+  }
+
+  public void logout() {
+    driver.findElement(By.linkText("Logout")).click();
+  }
+
+  public void returnToHomePage() {
+    driver.findElement(By.linkText("home")).click();
+  }
+
+  public void submitContactCreation() {
+    driver.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+  }
+
+  public void gotoAddNewContact() {
+    driver.findElement(By.linkText("add new")).click();
+  }
+
+  public boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  public boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  public void deleteSelectedContact() {
+    driver.findElement(By.xpath("//input[@value='Delete']")).click();
+    assertTrue(closeAlertAndGetItsText().matches("^Delete 1 addresses[\\s\\S]$"));
+  }
+
+  public void selectContact() {
+    driver.findElement(By.xpath("//td/input")).click();
+    acceptNextAlert = true;
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
   }
 }
