bd79ca0b59391b8ca320f3eed617830c64a9e2d4
egaraseva <https://stash.esc-hq.ru/login>
2018-09-14 11:16:43 +0300

    homework 4: new testContactCreation

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -0,0 +1,114 @@
+package ru.stqa.pft.addressbook;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.fail;
+
+public class ContactCreationTests {
+  private WebDriver driver;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @BeforeMethod(alwaysRun = true)
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    driver.get("http://localhost:8080/addressbook/addressbook/");
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
+    driver.findElement(By.name("user")).click();
+    driver.findElement(By.name("user")).sendKeys(username);
+    driver.findElement(By.name("pass")).click();
+    driver.findElement(By.name("pass")).sendKeys(password);
+    driver.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+  @Test
+  public void testContactCreation() throws Exception {
+    gotoAddNewContact();
+    fillContactForm(new ContactData("Michael", "Scott", "Dunder Mifflin", "02012345678", "m.scott@gmail.com", "Green Street, 14, Scranton"));
+    submitContactCreation();
+    returnToHomePage();
+    logout();
+  }
+
+  private void fillContactForm(ContactData contactData) {
+    driver.findElement(By.name("firstname")).click();
+    driver.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
+    driver.findElement(By.name("lastname")).click();
+    driver.findElement(By.name("lastname")).sendKeys(contactData.getSurname());
+    driver.findElement(By.name("company")).click();
+    driver.findElement(By.name("company")).sendKeys(contactData.getCompany());
+    driver.findElement(By.name("mobile")).click();
+    driver.findElement(By.name("mobile")).sendKeys(contactData.getPhone());
+    driver.findElement(By.name("email")).click();
+    driver.findElement(By.name("email")).sendKeys(contactData.getEmail());
+    driver.findElement(By.name("address2")).click();
+    driver.findElement(By.name("address2")).sendKeys(contactData.getAddress());
+  }
+
+  private void logout() {
+    driver.findElement(By.linkText("Logout")).click();
+  }
+
+  private void returnToHomePage() {
+    driver.findElement(By.linkText("home")).click();
+  }
+
+  private void submitContactCreation() {
+    driver.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+  }
+
+  private void gotoAddNewContact() {
+    driver.findElement(By.linkText("add new")).click();
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,43 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String firstName;
+  private final String surname;
+  private final String company;
+  private final String phone;
+  private final String email;
+  private final String address;
+
+  public ContactData(String firstName, String surname, String company, String phone, String email, String address) {
+    this.firstName = firstName;
+    this.surname = surname;
+    this.company = company;
+    this.phone = phone;
+    this.email = email;
+    this.address = address;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getSurname() {
+    return surname;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+}
