2b002fa66185f3e748e4c97ae3628504d51a01c9
egaraseva <https://stash.esc-hq.ru/login>
2018-11-03 15:01:41 +0300

    added FtpHelper, new tests Login and Registration (draft version)

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -5,42 +5,70 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
-import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
 
-  public Properties properties;
-  WebDriver driver;
+  private final Properties properties;
+  private WebDriver driver;
+  private FtpHelper ftp;
 
   private String browser;
+  private RegistrationHelper registrationHelper;
 
   public ApplicationManager(String browser) throws IOException {
     this.browser = browser;
+    properties = new Properties();
   }
 
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
-    properties = new Properties();
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-
-    if (browser.equals(BrowserType.FIREFOX)) {
-      driver = new FirefoxDriver();
-    } else if (browser.equals(BrowserType.GOOGLECHROME)) {
-      driver = new ChromeDriver();
-    } else if (browser.equals(BrowserType.IEXPLORE)) {
-      driver = new InternetExplorerDriver();
-    }
-
-    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-    driver.get(properties.getProperty("web.baseUrl"));
   }
 
   public void stop() {
-    driver.quit();
+    if (driver != null){
+      driver.quit();
+    }
+  }
+
+  public HttpSession newSession() {
+    return new HttpSession(this);
+  }
+
+  public String getProperty(String key) {
+    return properties.getProperty(key);
+  }
+
+  public RegistrationHelper registration() {
+    if (registrationHelper == null){
+      registrationHelper = new RegistrationHelper(this);
+    }
+    return registrationHelper;
+  }
+
+  public WebDriver getDriver() {
+    if (driver == null){
+      if (browser.equals(BrowserType.FIREFOX)) {
+        driver = new FirefoxDriver();
+      } else if (browser.equals(BrowserType.GOOGLECHROME)) {
+        driver = new ChromeDriver();
+      } else if (browser.equals(BrowserType.IEXPLORE)) {
+        driver = new InternetExplorerDriver();
+      }
+    }
+    return driver;
+  }
+
+  public FtpHelper ftp() {
+    if (ftp == null) {
+      ftp = new FtpHelper(this);
+    }
+    return ftp;
   }
 
 }
