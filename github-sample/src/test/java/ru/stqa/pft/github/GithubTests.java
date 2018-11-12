d1d668f1109fcf9509ddfc97dd9cdaa600e546fd
egaraseva <https://stash.esc-hq.ru/login>
2018-11-12 18:49:28 +0300

    new test GithubTests

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+  @Test
+  public void testCommits() throws IOException {
+    Github github = new RtGithub("a0840f6280c7fbc90e224eea1992272a36ec0849 ");
+    RepoCommits commits = github.repos().get(new Coordinates.Simple("elgaraseva", "java_course")).commits();
+    for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+      System.out.println(new RepoCommit.Smart(commit).message());
+    }
+  }
+}
